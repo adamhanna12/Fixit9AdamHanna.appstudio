bb29a444ea2e9c98333f3884e62dc3277eee811e
Adam <adamhanna@creighton.ed>
2019-10-11 13:02:20 -0500

    Membership Commit

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,15 @@
-
+/* 
 var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
 
+btSubmit.onclick=function(){
+  name = inptName.value
+  if (name  === "Mary" || name  === "Sam" || name  === "Joey" || name  === "Paul" || name  === "Mike" || name  === "Char" || name  === "Jenny" || name  === "Kennedy" || name  === "Kaden" || name  === "Kurt" || name  === "Anne") {
+    lblSubmit.value = `Welcome back, ${name}!`
+  } else  { 
+    lblSubmit.value = "You're being added as a member." 
+    members.push(name)
+    } 
+}
 
+console.log(members)
+*/ 
