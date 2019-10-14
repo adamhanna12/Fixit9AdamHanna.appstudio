556464d7265d5c4321268a3893ef953ed0af0029
Adam <adamhanna@creighton.ed>
2019-10-13 21:32:42 -0500

    Final Commit

---
+++ forms/calculator/calculator.js
@@ -0,0 +1,18 @@
+/*
+let number1 = (Number(""))
+let number2 = (Number(""))
+
+btnMultiply.onclick=function(){
+  number1 = inptNumber1.value
+  number2 = inptNumber2.value
+  lblCalc.value = `${number1 * number2}`
+}
+
+btnAdd.onclick=function(){
+  number1 = inptNumber1.value
+  number2 = inptNumber2.value
+  lblCalc.value = `${Number(number1) + Number(number2)}`
+} 
+*/
+
+
---
+++ forms/twoControls/twoControls.js
@@ -0,0 +1,11 @@
+/*
+btnRadio.onclick=function(){
+  lblRadio.value = "I changed the items in the radio button." 
+}
+
+
+
+togToggle.onclick=function(){
+  lblToggle.value = "I changed the appearance of the toggle to danger."
+}
+*/
